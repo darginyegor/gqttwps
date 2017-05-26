a3a044d170a60def44f136fe2de93816775d73dc
Yegor Dargin <darginyegor@gmail.com>
2017-05-26 14:41:10 +0600

    now with the help of Yandex.XML everything is almost ok. caching works too

--- main.py
+++ main.py
@@ -12,39 +12,46 @@ class App(QtWidgets.QApplication):
         self.ui = Ui()
         self.ui.show()
         self.ui.getButton.clicked.connect(self.on_get_button_click)
+        try:
+            f = open('api.cfg')
+            config = [line for line in f]
+            self.ui.apiKeyInput.setText(config[0])
+            self.ui.usernameInput.setText(config[1])
+        except FileNotFoundError:
+            pass
 
+    # Обработчик нажатия на кнопку "Получить"
     def on_get_button_click(self):
         query = self.ui.queryInput.text()
         min_size = str(self.ui.sizeSelector.currentText()).split('x')
-        self.get_by_query(query, min_size)
+        api_key = self.ui.apiKeyInput.text()
+        username = self.ui.usernameInput.text()
+        self.get_by_query(query, min_size, api_key, username)
 
     # Получение и кэширование картинок по запросу
-    def get_by_query(self, q, min_size):
-        url = 'http://yandex.ru/images/search?text='+q+'&isize=eq&iw='+min_size[0]+'&ih='+min_size[1]
-        print(url)
+    def get_by_query(self, q, min_size, api_key, username):
+        url = 'http://yandex.ru/images/search/xml?user=al'+username+'&key='+api_key+'&text='+q+'&isize=eq&iw='+min_size[0]+'&ih='+min_size[1]
+        print('Получение списка URL-адресов...')
         response = requests.get(url, verify=False)
         soup = BeautifulSoup(response.text, "lxml")  # .find(class_='serp-list')
-        print(soup)
         data_list = [i['data-bem'] for i in soup.find_all(class_='serp-item')]
         pictures = []
         for item in data_list[:10]:
             data = json.loads(item)
             pictures.append(data['serp-item']['img_href'])
+        print('Кэширование изображений...')
         self.cache(pictures)
 
-
+    # Кэширование полученных изображений
     def cache(self, list_of_urls):
         for url in list_of_urls:
-            hash = str(time.time())
-            ext = url[3:]
-            filename = 'cached/'+hash+'.'+ext
+            name = str(len(url))  # str(time.time())
+            ext = url[-3:]
+            filename = 'cached/'+name+'.'+ext
             image = requests.get(url, verify=False).content
-            f = open(filename, 'wb')
+            f = open(filename, 'wb+')
             f.write(image)
             f.close()
-
-
-
 
 
 # Класс графического интерфейса
